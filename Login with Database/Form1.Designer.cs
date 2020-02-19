09e41de1d61782fdd9a06af8a24f66aeb618d86c
JesseStokes <53128286+JesseStokes@users.noreply.github.com>
2020-02-19 17:35:19 -0600

    Add files via upload

---
+++ Login with Database/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Login_with_Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btLogin);
+            this.panel1.Controls.Add(this.tbPassword);
+            this.panel1.Controls.Add(this.tbUsername);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(31, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(725, 373);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Turquoise;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 141);
+            this.panel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(250, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(217, 109);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUsername.Location = new System.Drawing.Point(203, 173);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(317, 23);
+            this.tbUsername.TabIndex = 1;
+            this.tbUsername.Text = "Username / Email";
+            this.tbUsername.Enter += new System.EventHandler(this.tbUserEnter);
+            this.tbUsername.Leave += new System.EventHandler(this.tbUserLeave);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(203, 210);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(317, 23);
+            this.tbPassword.TabIndex = 2;
+            this.tbPassword.Text = "Password";
+            this.tbPassword.Enter += new System.EventHandler(this.tbPassEnter);
+            this.tbPassword.Leave += new System.EventHandler(this.tbPassLeave);
+            // 
+            // btLogin
+            // 
+            this.btLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btLogin.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLogin.ForeColor = System.Drawing.Color.White;
+            this.btLogin.Location = new System.Drawing.Point(250, 273);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(217, 36);
+            this.btLogin.TabIndex = 3;
+            this.btLogin.Text = "LOG IN";
+            this.btLogin.UseVisualStyleBackColor = false;
+            this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btLogin;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Login with Database/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Login_with_Database
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+
+        public Form1()
+        {
+            InitializeComponent();
+            con.ConnectionString = @"Data Source = DESKTOP-71Q98DC; Initial Catalog = LoginScreen; Integrated Security = True";
+        }
+
+        private void tbUserEnter(object sender, EventArgs e)
+        {
+            if (tbUsername.Text.Equals(@"Username / Email"))
+            {
+                tbUsername.Text = "";
+            }
+        }
+
+        private void tbUserLeave(object sender, EventArgs e)
+        {
+            if (tbUsername.Text.Equals(""))
+            {
+                tbUsername.Text = @"Username / Email";
+            }
+        }
+
+        private void tbPassEnter(object sender, EventArgs e)
+        {
+            if (tbPassword.Text.Equals("Password"))
+            {
+                tbPassword.Text = "";
+            }
+        }
+
+        private void tbPassLeave(object sender, EventArgs e)
+        {
+            if (tbPassword.Text.Equals(""))
+            {
+                tbPassword.Text = "Password";
+            }
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from Authorization";
+            SqlDataReader dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                if (tbUsername.Text.Equals(dr["username"].ToString()) && tbPassword.Text.Equals(dr["password"].ToString()))
+                {
+                    MessageBox.Show("Login Successful", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            con.Close();
+        }
+    }
+}
